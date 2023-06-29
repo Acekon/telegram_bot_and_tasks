1801866471aa0c6ca16bf1d79713f6f3a332295a
Acekon <acekon@gmail.com>
2023-06-29 14:28:17 +0300

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,14 @@
+import telebot
+import os
+
+bot_token  = f'{os.environ.get("tg_token")}'
+admin_id = f'{os.environ.get("tg_admin_id")}'
+send_chat_id = f'{os.environ.get("tg_id_channel")}'
+
+
+start_times = ['08:00']
+bot = telebot.TeleBot(bot_token)
+
+def db_path():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(dir_path, 'messages.db')
