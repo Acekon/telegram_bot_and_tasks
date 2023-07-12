b1569b8f3c0b0415377ae4c0461cd5014f5553ef
Acekon <acekon@gmail.com>
2023-07-12 09:04:16 +0300

    Update bot_task.py
    Added sending image for publication

--- bot_task.py
+++ bot_task.py
@@ -9,6 +9,7 @@ from conf import start_times, db_path, send_chat_id, bot, admin_id
 
 
 def send_message(send_chat_id):
+    print(f"Debug {(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))} sending")
     conn = sqlite3.connect(db_path())
     c = conn.cursor()
     c.execute('SELECT * FROM messages WHERE last_send is NULL')
@@ -21,13 +22,41 @@ def send_message(send_chat_id):
     messages_db = c.fetchall()
     message_db = messages_db[random.randint(0, len(messages_db) - 1)]
     message_id, text, last_sent = message_db
-    #debug_text = f"{message_id}, {text}, {last_sent},{(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}"
-    bot.send_message(chat_id=send_chat_id, text=f'{text}')
+    img_mess = open_image(message_id)
+    if img_mess:
+        send_photo(text, img_mess=img_mess)
+    else:
+        send_text(text)
     c.execute('UPDATE messages SET last_send=? WHERE ids=?', (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'), message_id))
     conn.commit()
     conn.close()
 
 
+def send_text(text):
+    bot.send_message(chat_id=send_chat_id, text=f'{text}')
+
+
+def send_photo(text, img_mess):
+    bot.send_photo(chat_id=send_chat_id, photo=open(img_mess, 'rb'), caption=f'{text}')
+
+
+def open_image(message_id):
+    source_dir = 'img/'
+    img_files = []
+    for path, dirs, files in os.walk(source_dir):
+        for filename in files:
+            fullpath = os.path.join(path, filename)
+            if int(filename.split('_')[0]) == message_id:
+                img_files.append(fullpath)
+            else:
+                if len(img_files) >= 1:
+                    break
+                continue
+    if len(img_files) != 0:
+        img = img_files[random.randrange(0, len(img_files))]
+        return img
+    else:
+        return False
 
 
 def run():
