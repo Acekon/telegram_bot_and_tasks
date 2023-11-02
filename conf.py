32054e2b3d4b4a2541ed435b530a1bbbf8991d8c
Serhii <acekon@gmail.com>
2023-11-02 10:10:48 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,10 +1,12 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-session = None
+from aiogram.client.session.aiohttp import AiohttpSession
+
+session = AiohttpSession(proxy="http://proxy2:3128/")
 start_times = ['08:00', '12:00']
-bot_token = os.getenv('TG_TOKEN')
+bot_token = ''
+admin_id = f'{os.environ.get("tg_admin_id")}'
+send_chat_id = f'{os.environ.get("tg_id_channel")}'
 
 
 def db_path():
